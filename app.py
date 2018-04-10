3103a3f6b2e9e5a6dbf5324215996a25d37150fa
Tongyan <tongyan@umich.edu>
2018-04-10 14:32:27 -0400

    Football

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 import model
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -10,12 +11,48 @@ def index():
         <h1>Michigan Sports Info!</h1>
         <ul>
             <li><a href="/bball"> Men's Basketball </a></li>
+            <li><a href="/fball"> Men's Football </a></li>
         </ul>
     '''
 
-@app.route('/bball')
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+    else:
+        firstname = ''
+        lastname = ''
+
+    return render_template("hello.html", firstname=firstname, lastname=lastname)
+
+
+@app.route('/bball', methods=['GET', 'POST'])
 def bball():
-    return render_template("seasons.html", seasons=model.get_bball_seasons())
+    model.init_ball('umbball.csv')
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_ball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_ball_seasons()
+
+    return render_template("seasons.html", seasons=seasons, ball='bball')
+
+
+@app.route('/fball', methods=['GET', 'POST'])
+def fball():
+    model.init_ball('umfootball.csv')
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_ball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_ball_seasons()
+
+    return render_template("seasons.html", seasons=seasons, ball='fball')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -2,12 +2,44 @@
 import csv
 
 BB_FILE_NAME = 'umbball.csv'
+FB_FILE_NAME = 'umfootball.csv'
 
-bb_seasons = []
+b_seasons = []
 
-def init_bball(csv_file_name=BB_FILE_NAME):
-    pass
 
-def get_bball_seasons(sortby='year', sortorder='desc'):
-    global bb_seasons
-    return bb_seasons
+def init_ball(csv_file_name):
+    global b_seasons
+    b_seasons = []  # reset, start clean
+
+    with open(csv_file_name) as f:
+        reader = csv.reader(f)
+        if csv_file_name == BB_FILE_NAME:
+            next(reader)  # throw away headers
+            next(reader)  # throw away headers
+            raw = list(reader)
+        elif csv_file_name == FB_FILE_NAME:
+            next(reader)  # throw away headers
+            raw = list(reader)
+        else:
+            raw = []
+
+    for r in raw:
+        if csv_file_name == BB_FILE_NAME:
+            b_seasons.append([r[1], int(r[3]), int(r[4]), float(r[5])])
+        elif csv_file_name == FB_FILE_NAME:
+            b_seasons.append([r[1], int(r[3]), int(r[4]), float(r[6])])
+
+
+def get_ball_seasons(sortby='year', sortorder='desc'):
+    if sortby == 'year':
+        sortcol = 0
+    elif sortby == 'wins':
+        sortcol = 1
+    elif sortby == 'pct':
+        sortcol = 3
+    else:
+        sortcol = 0
+
+    rev = (sortorder == 'desc')
+    sorted_list = sorted(b_seasons, key=lambda row: row[sortcol], reverse=rev)
+    return sorted_list
